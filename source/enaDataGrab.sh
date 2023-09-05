52c55b8e7c9c0ddaca4bd27d400271a2755a29f6
woollard <woollard@ebi.ac.uk>
2023-09-05 18:35:19 +0100

    capturing the searches in separate files

--- source/enaDataGrab.sh
+++ source/enaDataGrab.sh
@@ -9,16 +9,22 @@ outfile=$datadir/ena_study_searchable_fields.txt
 echo "generating "$outfile
 # curl -s -X 'GET'  'https://www.ebi.ac.uk/ena/portal/api/searchFields?dataPortal=ena&result=study&format=tsv' > $outfile
 
-outfile=$datadir/ena_study_environment_fields.txt
-echo "many potential examples found here: "$outfile
-echo "edna"
+field="edna"
+outfile=$datadir"/ena_study_"$field"_fields.txt"
+echo $field" "$outfile
 curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=study&query=study_name%3D%22eDNA%22%20OR%20study_title%3D%22eDNA%22%20OR%20study_description%3D%22eDNA%22&fields=study_accession%2Cstudy_title%2Cstudy_name&format=tsv' "https://www.ebi.ac.uk/ena/portal/api/search" > $outfile
-echo "metabarcoding"
-curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=study&query=study_name%3D%22metabarcoding%22%20OR%20study_title%3D%22metabarcoding%22%20OR%20study_description%3D%22metabarcoding%22&fields=study_accession%2Cstudy_title%2Cstudy_name&format=tsv' "https://www.ebi.ac.uk/ena/portal/api/search" >> $outfile
-echo "barcoding"
-curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=study&query=study_name%3D%22barcoding%22%20OR%20study_title%3D%22barcoding%22%20OR%20study_description%3D%22barcoding%22&fields=study_accession%2Cstudy_title%2Cstudy_name&format=tsv' "https://www.ebi.ac.uk/ena/portal/api/search" >> $outfile
-echo "environment"
-curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=study&query=study_name%3D%22environment%22%20OR%20study_title%3D%22environment%22%20OR%20study_description%3D%22environment%22&fields=study_accession%2Cstudy_title%2Cstudy_name&format=tsv' "https://www.ebi.ac.uk/ena/portal/api/search" >> $outfile
+field="metabarcoding"
+outfile=$datadir"/ena_study_"$field"_fields.txt"
+echo $field" "$outfile
+curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=study&query=study_name%3D%22metabarcoding%22%20OR%20study_title%3D%22metabarcoding%22%20OR%20study_description%3D%22metabarcoding%22&fields=study_accession%2Cstudy_title%2Cstudy_name&format=tsv' "https://www.ebi.ac.uk/ena/portal/api/search" > $outfile
+field="barcoding"
+outfile=$datadir"/ena_study_"$field"_fields.txt"
+echo $field" "$outfile
+curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=study&query=study_name%3D%22barcoding%22%20OR%20study_title%3D%22barcoding%22%20OR%20study_description%3D%22barcoding%22&fields=study_accession%2Cstudy_title%2Cstudy_name&format=tsv' "https://www.ebi.ac.uk/ena/portal/api/search" > $outfile
+field="environment"
+outfile=$datadir"/ena_study_"$field"_fields.txt"
+echo $field" "$outfile
+curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=study&query=study_name%3D%22environment%22%20OR%20study_title%3D%22environment%22%20OR%20study_description%3D%22environment%22&fields=study_accession%2Cstudy_title%2Cstudy_name&format=tsv' "https://www.ebi.ac.uk/ena/portal/api/search" > $outfile
 
 #Targetted capture, but not specifically environment
 #curl -X POST -H "Content-Type: application/x-www-form-urlencoded" -d 'result=read_experiment&query=library_strategy%3D%22Targeted-Capture%22&fields=experiment_accession%2Cexperiment_title%2Clibrary_strategy&format=tsv' "https://www.ebi.ac.uk/ena/portal/api/search"
