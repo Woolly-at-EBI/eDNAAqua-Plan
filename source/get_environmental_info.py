894522afa764beae24d5e055ee39d12e6cbd376a
woollard <woollard@ebi.ac.uk>
2024-05-20 19:02:53 +0100

    major update...  improved the search to pull in NCBI and DDBJ submitted samples, adding in barcode gene searching.

--- source/get_environmental_info.py
+++ source/get_environmental_info.py
@@ -33,9 +33,14 @@ def get_query_params():
                  '%20OR%20CHECKLIST%3D%22ERC000021%22%20OR%20CHECKLIST%3D%22ERC000022%22%20OR%20CHECKLIST%3D'
                  '%22ERC000023%22%20OR%20CHECKLIST%3D%22ERC000024%22%20OR%20CHECKLIST%3D%22ERC000025%22%20OR'
                  '%20CHECKLIST%3D%22ERC000027%22%20OR%20CHECKLIST%3D%22ERC000055%22%20OR%20CHECKLIST%3D%22ERC000030'
-                 '%22%20OR%20CHECKLIST%3D%22ERC000031%22%20OR%20CHECKLIST%3D%22ERC000036%22))%20AND%20not_tax_tree('
+                 '%22%20OR%20CHECKLIST%3D%22ERC000031%22%20OR%20CHECKLIST%3D%22ERC000036%22)%20OR'
+                 '(ncbi_reporting_standard%3D%22*ENV*%22%20ORncbi_reporting_standard%3D%22*WATER*%22'
+                 '%20ORncbi_reporting_standard%3D%22*SOIL*%22%20ORncbi_reporting_standard%3D%22*AIR*%22'
+                 '%20ORncbi_reporting_standard%3D%22*SEDIMENT*%22%20ORncbi_reporting_standard%3D%22*BUILT%22%20))'
+                 'AND%20not_tax_tree('
                  '9606)'
     }
+
     return my_params
 
 
@@ -110,13 +115,14 @@ def get_env_readrun_ids():
 
 
 def get_env_readrun_detail():
-    env_read_run_detail_file = "read_run_detail.pickle"
+    env_read_run_detail_file = "read_run_ena_detail.pickle"
+    env_read_run_detail_file = "read_run_allinsdc_detail.pickle"
     if os.path.exists(env_read_run_detail_file):
         ic(f"{env_read_run_detail_file} exists, so can unpickle it")
         return unpickle_data_structure(env_read_run_detail_file)
 
     fields = ("sample_accession%2Crun_accession%2Clibrary_strategy%2Clibrary_source%2Cinstrument_platform%2Clat%2Clon%2Ccountry"
-              "%2Cbroad_scale_environmental_context%2Ctax_id%2Cchecklist%2Ccollection_date%2Ctarget_gene%2Ctag")
+              "%2Cbroad_scale_environmental_context%2Ctax_id%2Cchecklist%2Ccollection_date%2Cncbi_reporting_standard%2Ctarget_gene%2Ctag%2Cstudy_accession%2Cstudy_title")
 
     query_params_json = get_query_params()
     srv = query_params_json['srv']
@@ -134,6 +140,26 @@ def get_env_readrun_detail():
     pickle_data_structure(output, env_read_run_detail_file)
     return output
 
+def get_all_study_details():
+    study_details_file = "study_details.pickle"
+    if os.path.exists(study_details_file):
+        ic("env_sample_id_file exists, so can unpickle it")
+        record_list = unpickle_data_structure(study_details_file)
+        return pd.DataFrame.from_records(record_list)
+
+    #'result=study&fields=study_accession%2Cstudy_title%2Cstudy_description&format=tsv'
+    query_params_json = get_query_params()
+    srv = query_params_json['srv']
+    fields = ("study_accession%2Cstudy_title%2Cstudy_description")
+    params = "result=study" + "&fields=" + fields + "&format=json"
+    limit = '&limit=0'
+    url = srv + '?' + params + limit
+    ic(url)
+    record_list = json.loads(run_webservice(url))
+    #ic(record_list)
+
+    pickle_data_structure(record_list, study_details_file)
+    return pd.DataFrame.from_records(record_list)
 
 def get_env_sample_ids():
     env_sample_id_file = "env_sample_id_file.pickle"
@@ -244,7 +270,7 @@ def collection_date_year(value):
         else:
             return '20' + str(extract_value)
     else:
-        ic(f"no year match for {value}")
+        #ic(f"no year match for {value}")  # e,g,  f"no year match for {value}": 'no year match for restricted access'
         return ""
 
 def create_year_bins(value):
@@ -412,6 +438,8 @@ def experimental_analysis_inc_filtering(df):
 def target_gene_analysis(df):
     ic(df['target_gene'].value_counts().head())
 
+
+
 def plot_sunburst(df, title, path_list, value_field, plotfile):
     """
 
@@ -473,6 +501,7 @@ def analyse_environment(df):
     multiples = []
     aquatic_tag_set = ['env_geo:marine', 'env_geo:freshwater', 'env_geo:brackish', 'env_geo:coastal', 'env_tax:marine',
                         'env_tax:freshwater', 'env_tax:brackish', 'env_tax:coastal']
+    terrestrial_tag_set = ['env_geo:terrestrial', 'env_tax:terrestrial']
     for tags in tmp_df['env_tag_string'].unique():
         tag_list = tags.split(';')
         # ic(tags)
@@ -495,7 +524,7 @@ def analyse_environment(df):
                 else:
                     ic(msg)
                     multiples.append(msg)
-            else:
+            else:  # ie. one match
                 if matches[0] == 'env_geo:marine' and 'env_tax:marine' in tags:
                     tag_string_assignment[tags] = {'prediction': 'marine', 'confidence': 'high'}
                 elif matches[0] == 'env_geo:freshwater' and 'env_geo:freshwater' in tags:
@@ -509,9 +538,13 @@ def analyse_environment(df):
                 elif len(tag_list) == 2 and 'env_geo:terrestrial' not in tags:
                     if tag_list[0] in aquatic_tag_set and tag_list[1] in aquatic_tag_set:
                         tag_string_assignment[tags] = {'prediction': 'mixed_aquatic', 'confidence': 'medium'}
+                    elif (tag_list[0] in terrestrial_tag_set and tag_list[1] in aquatic_tag_set) or (tag_list[1] in terrestrial_tag_set and tag_list[0] in aquatic_tag_set):
+                            tag_string_assignment[tags] = {'prediction': 'mixed', 'confidence': 'low'}
                 elif len(tag_list) == 3 and 'env_geo:terrestrial' not in tags:
                     if tag_list[0] in aquatic_tag_set and (tag_list[1] in aquatic_tag_set or tag_list[2] in aquatic_tag_set):
                         tag_string_assignment[tags] = {'prediction': 'mixed_aquatic', 'confidence': 'medium'}
+                    else:
+                        tag_string_assignment[tags] = {'prediction': 'mixed', 'confidence': 'low'}
                 else:
                     ic("________________________________________________________")
 
@@ -521,19 +554,30 @@ def analyse_environment(df):
                         tag_string_assignment[tags] = {'prediction': value, 'confidence': 'medium'}
                     elif matches[0] == 'env_geo:coastal' and "brackish" in tags:
                         tag_string_assignment[tags] = {'prediction': 'coastal', 'confidence': 'medium'}
+                    elif matches[0] == 'env_geo:marine' and ("brackish" in tags or "coastal" in tags):
+                        tag_string_assignment[tags] = {'prediction': 'coastal', 'confidence': 'medium'}
+                    elif matches[0] == 'env_geo:marine' and ("terrestrial" in tags):
+                        tag_string_assignment[tags] = {'prediction': 'mixed', 'confidence': 'low'}
                     else:
                         if re.match(r'^(env_tax:freshwater;env_geo:marine|env_tax:freshwater;env_tax:terrestrial;env_geo:marine)$',tags):
                             not_assigned.append(tags)
                         else:
                             ic(f"Not assigned--->{tags} len_tags={len(tag_list)}")
                             sys.exit()
+
+        # the following are where there are no env_geo: tgs
         elif len(tag_list) == 1:
             value = re.findall(r'env_tax:(.*)', tag_list[0])[0]
             tag_string_assignment[tags] = {'prediction': value, 'confidence': 'medium'}
         elif len(tag_list) == 2:
             if tag_list[0] in aquatic_tag_set and tag_list[1] in aquatic_tag_set:
                 tag_string_assignment[tags] = {'prediction': 'mixed_aquatic', 'confidence': 'medium'}
-            elif tag_list[0] in aquatic_tag_set or tag_list[1] in aquatic_tag_set:
+            elif tag_list[0] in aquatic_tag_set and tag_list[1] in aquatic_tag_set:
+                tag_string_assignment[tags] = {'prediction': 'mixed', 'confidence': 'low'}
+            elif tag_list[0] in aquatic_tag_set and tag_list[1] in terrestrial_tag_set:
+                tag_string_assignment[tags] = {'prediction': 'mixed', 'confidence': 'low'}
+            elif ((tag_list[0] in terrestrial_tag_set and tag_list[1] in aquatic_tag_set) or
+                  (tag_list[1] in terrestrial_tag_set and tag_list[0] in aquatic_tag_set)):
                 tag_string_assignment[tags] = {'prediction': 'mixed', 'confidence': 'low'}
             else:
                 not_assigned.append(tags)
@@ -544,6 +588,9 @@ def analyse_environment(df):
                 elif tag_list[1] in aquatic_tag_set and (
                         tag_list[2] in aquatic_tag_set):
                     tag_string_assignment[tags] = {'prediction': 'mixed_aquatic', 'confidence': 'low'}
+                elif tag_list[0] in terrestrial_tag_set and (
+                            tag_list[1] in aquatic_tag_set and tag_list[2] in aquatic_tag_set):
+                    tag_string_assignment[tags] = {'prediction': 'mixed', 'confidence': 'low'}
                 else:
                     not_assigned.append(tags)
         elif len(tag_list) == 4:
@@ -703,10 +750,26 @@ def clean_dates_in_df(df):
 
     return df
 
+def add_insdc_member_receiver(df):
+    ic()
+    #df = df.sample(n=100)
+    ic(df.dtypes)
+    def get_insdc_member_receiver(value):
+        if value.startswith('SAMN'):
+            return 'NCBI'
+        elif value.startswith('SAME'):
+            return 'ENA'
+        elif value.startswith('SAMD'):
+            return 'DDBJ'
+        else:
+            return None
+
+    df['insdc_member_receiver'] = df['sample_accession'].apply(get_insdc_member_receiver)
+    ic()
+    return df
 
 
 def analyse_readrun_detail(env_readrun_detail):
-    ic(len(env_readrun_detail))
     # count = 0
     # for record in env_readrun_detail:
     #
@@ -716,12 +779,38 @@ def analyse_readrun_detail(env_readrun_detail):
     #         break
 
     df = pd.DataFrame.from_records(env_readrun_detail)
+    ic(df.columns)
+    # ['sample_accession', 'run_accession', 'library_strategy',
+    #                        'library_source', 'instrument_platform', 'lat', 'lon', 'country',
+    #                        'broad_scale_environmental_context', 'tax_id', 'checklist',
+    #                        'collection_date', 'ncbi_reporting_standard', 'target_gene', 'tag']
+    # dtype = 'object')
+    ic()
+    # df = df.sample(n=100000)
+    # df['sample_accession'] = df['sample_accession'].to_string()
+    ic(df['sample_accession'])
+    df = add_insdc_member_receiver(df)
+    print_value_count_table(df.insdc_member_receiver)
+
+    # outfile = all_sample_accessions.tsv"
+    # ic(outfile)
+    # df.sample_accession.to_csv(outfile)
+    target_gene_analysis(df)
+
+    sys.exit()
+    print('NCBI "checklists":')
+    print_value_count_table(df.ncbi_reporting_standard)
+    print('ENA "checklists":')
+    print_value_count_table(df.checklist)
+
+
+
+
+    sys.exit()
     df = clean_dates_in_df(df)
 
     df['lat'] = pd.to_numeric(df['lat'], errors = 'coerce')
     df['lon'] = pd.to_numeric(df['lon'], errors = 'coerce')
-
-
 
     df = experimental_analysis_inc_filtering(df)
     print_value_count_table(df.collection_year)
@@ -733,14 +822,59 @@ def analyse_readrun_detail(env_readrun_detail):
     ic(df)
     ic(df.dtypes)
 
+def analyse_all_study_details(df):
+    """
+    Generates a subset of the df, indexed from sample_accession
+    Plus annotates two columns using
+    barcoding_df['barcoding_genes_from_study'] = list of genes
+    barcoding_df['is_barcoding_experiment_probable'] = True
+
+    :param df:
+    :return: barcoding_df
+    """
+    ic(len(df))
+    barcoding_pattern = '16S|18S|ITS|26S|5.8S|RBCL|rbcL|matK|MATK|COX1|CO1|mtCO|barcod'
+    barcoding_title_df = df[df.study_title.str.contains(barcoding_pattern, regex= True, na=False)]
+    ic(f"'study_title' with barcoding genes total={len(barcoding_title_df)}")
+    ic(barcoding_title_df['study_title'].sample(n=10))
+
+    barcoding_description_df = df[df.study_description.str.contains(barcoding_pattern, regex= True, na=False)]
+    ic(f"'study_description' with barcoding genes total={len(barcoding_description_df)}")
+    ic(barcoding_description_df['study_description'].sample(n=5))
+
+    barcoding_df = pd.concat([barcoding_title_df, barcoding_description_df]).drop_duplicates().reset_index(drop=True)
+    ic(f"barcoding total = {len(barcoding_df)}")
+    barcoding_df['combined_tit_des'] = barcoding_df['study_title'] + barcoding_df['study_description']
+    barcoding_df['is_barcoding_experiment_probable'] = True
+
+    def get_barcoding_genes(value):
+
+        genes = list(set(re.findall(r'16[Ss] ?rRNA|16[Ss] ?rDNA|18[Ss]|ITS|26[Ss]|5.8[Ss]|RBCL|matK|MATK|COX1|CO1|COI|mtCO', value)))
+        if len(genes) > 0:
+            # ic(genes)
+            return genes
+        else:
+            genes = list(set(re.findall(r'16[Ss]', value)))
+            if len(genes) > 0:
+                return genes
+            return None
+    barcoding_df['barcoding_genes_from_study'] = barcoding_df.combined_tit_des.apply(get_barcoding_genes)
+    ic(barcoding_df['barcoding_genes_from_study'].value_counts())
+    print_value_count_table(barcoding_df['barcoding_genes_from_study'])
+
+    return barcoding_df
 
 def main():
+    df_all_study_details = analyse_all_study_details(get_all_study_details())
+    ic(len(df_all_study_details))
+    sys.exit()
     sample_ids = get_env_sample_ids()
     ic(len(sample_ids))
     readrun_ids = get_env_readrun_ids()
     ic(len(readrun_ids))
 
     env_readrun_detail = get_env_readrun_detail()
+    ic(len(env_readrun_detail))
     analyse_readrun_detail(env_readrun_detail)
 
 
